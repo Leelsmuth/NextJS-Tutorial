a079b50b03b5ccb4ab7542b3634aa1c5d3d1345e
Oluwafemi Anjorin <leelsmuth@Oluwafemis-MBP.phub.net.cable.rogers.com>
2021-10-26 17:55:25 -0400

    conditonal for using style jsx

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,14 @@
 import headerStyles from '../styles/Header.module.css';
 
 const Header = () => {
+  const x = 7;
   return (
     <div>
       <h1 className="title"><span>WebDev</span> News</h1>
       <style jsx>
         {`
           .title {
-            color: red
+            color: ${x === 5 ? 'red' : 'blue'}
           }
         `}
       </style>
